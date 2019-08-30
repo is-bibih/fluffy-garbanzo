2bfe68edd3037049c3e32a34649c690390965999
is-bibih <vivianamaldes@gmail.com>
2019-08-29 22:34:52 -0500

    Define funciones para pruebas y arregla referencias.

--- nube.py
+++ nube.py
@@ -1,16 +1,16 @@
 class Nube(object):
-	def __init__(self, vol, forma, humedad, edad=1):
+	def __init__(self, vol, forma, humedad, edad=1, *args, **kwargs):
 		self.vol = vol
 		self.forma = forma
 		self.humedad = humedad
 		self.edad = edad
 
 	def llover(self):
-		if vol > 10 and humedad > 0.5:
+		if self.vol > 10 and self.humedad > 0.5:
 			print('está lloviendo')
 			self.vol -= 1
 			self.humedad -= 0.1
-		elif len(forma) < 6:
+		elif len(self.forma) < 6:
 			print('podría llover después')
 			self.vol += 1
 			self.humedad += self.humedad*0.7
@@ -18,22 +18,29 @@ class Nube(object):
 			print('nunca va a llover de esta nube')
 
 	def disipar(self):
-		if edad >= 5:
+		if self.edad >= 5:
 			print('se disipó la nube')
 		else:
 			print('la nube sigue ahí')
 			self.edad += 1
 
-print('nada tiene validación entonces por favor pon cosas razonables')
-vol = float(input('volumen de la nube (m^3): '))
-forma = input('cómo describirías la forma de la nube: ')
-humedad = float(input('proporción de la nube que es agua: '))
-edad = int(input('cuántos días tiene la nube: '))
 
-if not edad:
-	nube = Nube(vol, forma, humedad)
-else:
-	nube = Nube(vol, forma, humedad, edad)
+def pedir_valores():
+	print('nada tiene validación entonces por favor pon cosas razonables')
+	vol = float(input('volumen de la nube (m^3): '))
+	forma = input('cómo describirías la forma de la nube: ')
+	humedad = float(input('proporción de la nube que es agua: '))
+	edad = int(input('cuántos días tiene la nube: '))
 
-nube.llover()
-nube.disipar()
+	return (vol, forma, humedad, edad)
+
+def prueba(vol, forma, humedad, edad):
+	if not edad:
+		nube = Nube(vol, forma, humedad)
+	else:
+		nube = Nube(vol, forma, humedad, edad)
+
+	nube.llover()
+	nube.disipar()
+
+prueba(*pedir_valores())
