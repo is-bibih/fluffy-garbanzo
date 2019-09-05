b63da7222fc66db8dfd34cffdc2810d941d9f4f3
is-bibih <vivianamaldes@gmail.com>
2019-09-05 11:01:57 -0500

    Acaba archivo de K-NN.

--- intro-to-ml/ch2-knn-neighbors.py
+++ intro-to-ml/ch2-knn-neighbors.py
@@ -14,6 +14,14 @@ k-nearest neighbors (k-nn)
     - uses voting for k > 1 in classification (picks majority class)
     - calculates mean for k > 1 in regression
     - greater k results in simpler model (more general)
+    - main parameters are number of neighbors and how distance is measured
+        - it usually works well to choose 3-5
+        - euclidean distance is used by default (usually works)
+    - building is usually fast, but prediction can be slow with
+      large training datasets
+    - data should be preprocessed
+    - it usually does not do well with sparse datasets (most features are 0)
+    - it usually does not do well on datasets with many features (100+)
 decision boundary: the divide between where algorithm assigns different classes
 '''
 
@@ -95,3 +103,25 @@ reg.fit(X_train, y_train)
 print("Test set predictions:\n{}".format(reg.predict(X_test)))
 # evaluate with score (returns coefficient of determination R^2)
 print("Test set R^2: {:.2f}".format(reg.score(X_test, y_test)))
+
+# look at predictions using a test dataset (many points on the line)
+fig, axes = plt.subplots(1, 3, figsize=(15, 4))
+# 1000 data points between -3 and 3
+# reshape -1 means it adjusts to data
+line = np.linspace(-3, 3, 1000).reshape(-1, 1)
+# predict with 1, 3 or 9 neighbors
+for n_neighbors, ax in zip([1, 3, 9], axes):
+    reg = KNeighborsRegressor(n_neighbors=n_neighbors)
+    reg.fit(X_train, y_train)
+    ax.plot(line, reg.predict(line))
+    ax.plot(X_train, y_train, '^', c=mglearn.cm2(0), markersize=8)
+    ax.plot(X_test, y_test, 'v', c=mglearn.cm2(1), markersize=8)
+    ax.set_title(
+        "{} neighbor(s)\n train score: {:.2f} test score: {:.2f}".format(
+            n_neighbors, reg.score(X_train, y_train),
+            reg.score(X_test, y_test)))
+    ax.set_xlabel("Feature")
+    ax.set_ylabel("Target")
+axes[0].legend(["Model predictions", "Training data/target",
+                "Test data/target"], loc="best")
+# plt.show()
