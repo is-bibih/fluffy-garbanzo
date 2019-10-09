43fd97a89d6f496e8a67d06abe02346d522f7d8b
is-bibih <vivianamaldes@gmail.com>
2019-10-08 23:33:20 -0500

    Avanza en grid search.

--- intro-to-ml/ch5-4-grid-search.py
+++ intro-to-ml/ch5-4-grid-search.py
@@ -53,4 +53,36 @@ for gamma in [0.001, 0.01, 0.1, 1, 10, 100]:
 print("Best score: {:.2f}".format(best_score))
 print("Best parameters: {}".format(best_parameters))
 
-# grid search split into 
+# grid search with validation set
+
+# split data into train-validation and test
+X_trainval, X_test, y_trainval, y_test = train_test_split(
+    iris.data, iris.target, random_state=0)
+# split train-validation
+X_train, X_valid, y_train, y_valid = train_test_split(
+    X_trainval, y_trainval, random_state=1)
+print("size of training set: {}    size of validation set: {}" \
+      "    size of test set: {}\n".format(X_train.shape[0],
+                                          X_valid.shape[0],
+                                          X_test.shape[0]))
+
+best_score = 0
+
+for gamma in [0.001, 0.01, 0.1, 1, 10, 100]:
+    for C in [0.001, 0.01, 0.1, 1, 10, 100]:
+        svm = SVC(gamma=gamma, C=C)
+        svm.fit(X_train, y_train)
+        # evaluate SVC with validation set
+        score = svm.score(X_valid, y_valid)
+        # compare scores
+        if score > best_score:
+            best_score = score
+            best_parameters =  {'C': C, 'gamma': gamma}
+
+# build model with complete data and best parameters
+svm = SVC(**best_parameters)
+svm.fit(X_trainval, y_trainval)
+test_score = svm.score(X_test, y_test)
+print("best score on validation: {:.2f}".format(best_score))
+print("best parameters: {}".format(best_parameters))
+print("Test set score with best parameters: {:.2f}".format(test_score))
