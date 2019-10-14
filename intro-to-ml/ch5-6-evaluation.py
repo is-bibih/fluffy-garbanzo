27e1f5ffd0134deb7129cb869e85066d993bc0d3
is-bibih <vivianamaldes@gmail.com>
2019-10-13 20:56:57 -0500

    Avanza en el archivo de evaluación de modelos.

--- intro-to-ml/ch5-6-evaluation.py
+++ intro-to-ml/ch5-6-evaluation.py
@@ -7,6 +7,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.dummy import DummyClassifier
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
 
 '''
 evaluation metrics and scoring
@@ -31,6 +32,14 @@ metrics for binary classification
         - if a model gets only slightly better accuracy, it could
           indicate that there is something wrong with how the model
           is being used, or that accuracy is not a good measure
+
+confusion matrices
+    - can be used to represent the results of evaluating binary
+      classification
+    - the rows represent true classes
+    - the columns represent the class predictions made by the model
+    - entries on the main diagonal correspont to correct
+      clasifications (true positives and negatives)
 '''
 
 digits = load_digits()
@@ -47,15 +56,51 @@ def imbalanced(X_train, X_test, y_train, y_test):
     print("Unique predicted labels: {}".format(np.unique(pred_most_frequent)))
     print("Test score: {:.2f}".format(dummy_majority.score(X_test, y_test)))
 
+    return pred_most_frequent
+
 def decision_tree(X_train, X_test, y_train, y_test):
     tree = DecisionTreeClassifier(max_depth=2).fit(X_train, y_train)
     pred_tree = tree.predict(X_test)
     print("Test score: {:.2f}".format(tree.score(X_test, y_test)))
+
+    return pred_tree
 
 def other_dummy(X_train, X_test, y_train, y_test):
     dummy = DummyClassifier().fit(X_train, y_train)
     pred_dummy = dummy.predict(X_test)
     print("dummy score: {:.2f}".format(dummy.score(X_test, y_test)))
 
-imbalanced(X_train, X_test, y_train, y_test)
-decision_tree(X_train, X_test, y_train, y_test)
+    logreg = LogisticRegression(C=0.1).fit(X_train, y_train)
+    pred_logreg = logreg.predict(X_test)
+    print("logreg score: {:.2f}".format(logreg.score(X_test, y_test)))
+
+    return pred_dummy, pred_logreg
+
+def confuse(y_test, pred_logreg, show=False):
+    confusion = confusion_matrix(y_test, pred_logreg)
+    print("Confusion matrix:\n{}".format(confusion))
+
+    if show:
+        # show illustration
+        mglearn.plots.plot_confusion_matrix_illustration()
+        plt.show()
+        # show confusion matrix meaning
+        mglearn.plots.plot_binary_confusion_matrix()
+        plt.show()
+
+def compare(y_test, pred_most_frequent, pred_dummy, pred_tree,
+            pred_logreg):
+    print("Most frequent class:")
+    print(confusion_matrix(y_test, pred_most_frequent))
+    print("\nDummy model:")
+    print(confusion_matrix(y_test, pred_dummy))
+    print("\nDecision tree:")
+    print(confusion_matrix(y_test, pred_tree))
+    print("\nLogReg:")
+    print(confusion_matrix(y_test, pred_logreg))
+
+pred_most_frequent = imbalanced(X_train, X_test, y_train, y_test)
+pred_tree = decision_tree(X_train, X_test, y_train, y_test)
+pred_dummy, pred_logreg = other_dummy(X_train, X_test, y_train, y_test)
+confuse(y_test, pred_logreg)
+compare(y_test, pred_most_frequent, pred_dummy, pred_tree, pred_logreg)
