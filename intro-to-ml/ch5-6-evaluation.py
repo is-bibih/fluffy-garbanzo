36f4dc067b015748e177b443ff3047e6880e87b0
is-bibih <vivianamaldes@gmail.com>
2019-10-16 20:58:12 -0500

    Acaba archivo de evaluación de modelos.

--- intro-to-ml/ch5-6-evaluation.py
+++ intro-to-ml/ch5-6-evaluation.py
@@ -5,10 +5,12 @@ import mglearn
 from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
 from sklearn.dummy import DummyClassifier
+from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, f1_score, \
     classification_report
+from mglearn.datasets import make_blobs
 
 '''
 evaluation metrics and scoring
@@ -56,6 +58,24 @@ confusion matrices summaries:
         - it produces one line per class
         - support is the amount of samples according to ground truth
         - the last row shows a weighted average
+
+taking uncertainty into account
+    - most classifiers have a decision_function or a predict_proba
+      that access degrees of certainty about predictions
+    - by default, points with a decision_function > 0 are
+      classified as class 1
+        - if we want more points to be classified as 1, we
+          decrease the threshold
+        - if precision is valued over recall or the other way around,
+          or the data is heavily unbalanced, changing the decision
+          threshold is an easy way to improve results
+    - it can be easier to improve results for models with predict_proba
+      (it is fixed on a 0 to 1 scale)
+        - by default, the thresholf of 0.5 means that a point will
+          be positive if the model is 50% sure it is
+        - calibrated models provide accurate measures of uncertainty
+            - a fully grown decision tree is not calibrated,
+              it is always 100% sure of its decisions
 '''
 
 digits = load_digits()
@@ -67,9 +87,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 def imbalanced(X_train, X_test, y_train, y_test):
     # look at how dummy classifier gets great score
     dummy_majority = DummyClassifier(
-        strategy='most_frequent').fit(X_train, DummyClassifier(
         strategy='most_frequent').fit(X_train, y_train)
-    pred_most_frequent = dummy_majority.predict(X_test) y_train)
     pred_most_frequent = dummy_majority.predict(X_test)
     print("Unique predicted labels: {}".format(np.unique(pred_most_frequent)))
     print("Test score: {:.2f}".format(dummy_majority.score(X_test, y_test)))
@@ -141,10 +159,33 @@ def clas_report(y_test, pred_most_frequent, pred_dummy, pred_tree,
     print(classification_report(y_test, pred_logreg,
                                 target_names=["not nine", "nine"]))
 
-pred_most_frequent = imbalanced(X_train, X_test, y_train, y_test)
-pred_tree = decision_tree(X_train, X_test, y_train, y_test)
-pred_dummy, pred_logreg = other_dummy(X_train, X_test, y_train, y_test)
+def uncertainty():
+    # unbalanced binary classification (400 negative and 50 positive)
+    # plots decision threshold for a kernel svm model
+    X, y = make_blobs(n_samples=(400, 50), centers=2,
+        cluster_std=[7.0, 2], random_state=22)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, random_state=0)
+    svc = SVC(gamma=.05).fit(X_train, y_train)
+    mglearn.plots.plot_decision_threshold()
+    plt.show()
+
+    # use classification_report to evaluate precision and recall
+    # for both classes
+    print(classification_report(y_test, svc.predict(X_test)))
+    # there's a smaller recall and mixed precision for class 1,
+    # since the classifier focuses on 0 since it's much larger
+
+    y_pred_lower_threshold = svc.decision_function(X_test) > -0.8
+    print(classification_report(y_test, y_pred_lower_threshold))
+    # this makes the recall for class 1 go up and the precision
+    # goes down
+
+# pred_most_frequent = imbalanced(X_train, X_test, y_train, y_test)
+# pred_tree = decision_tree(X_train, X_test, y_train, y_test)
+# pred_dummy, pred_logreg = other_dummy(X_train, X_test, y_train, y_test)
 # confuse(y_test, pred_logreg)
 # compare(y_test, pred_most_frequent, pred_dummy, pred_tree, pred_logreg)
-f1_scores(y_test, pred_most_frequent, pred_dummy, pred_tree, pred_logreg)
-clas_report(y_test, pred_most_frequent, pred_dummy, pred_tree, pred_logreg)
+# f1_scores(y_test, pred_most_frequent, pred_dummy, pred_tree, pred_logreg)
+# clas_report(y_test, pred_most_frequent, pred_dummy, pred_tree, pred_logreg)
+uncertainty()
