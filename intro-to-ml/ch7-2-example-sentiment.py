a257da09428131272e2f445600ca1c2116bb1ca3
is-bibih <vivianamaldes@gmail.com>
2019-10-25 17:42:17 -0500

    Avanza en capítulo 7.

--- intro-to-ml/ch7-2-example-sentiment.py
+++ intro-to-ml/ch7-2-example-sentiment.py
@@ -32,6 +32,8 @@ bag-of-words representation
 processing
     - logreg usually works well with sparse high-dimensional data
       (like this)
+    - removing features with min_df can help make the model faster to
+      train and interpret (even if it doesn't improve performance)
 '''
 
 def load_texts():
@@ -62,6 +64,23 @@ def load_texts():
 
     return text_train, text_test, y_train, y_test
 
+def only_load():
+    # load bunch with data
+    reviews_train = load_files("data/aclImdb/train")
+    text_train, y_train = reviews_train.data, reviews_train.target
+
+    # clean data from its weird html breaks
+    text_train = [doc.replace(b"<br />", b" ") for doc in text_train]
+
+    # load test dataset
+    reviews_test = load_files("data/aclImdb/test/")
+    text_test, y_test = reviews_test.data, reviews_test.target
+
+    # clean
+    text_test = [doc.replace(b"<br />", b"  ") for doc in text_test]
+
+    return text_train, text_test, y_train, y_test
+
 def example_bag():
     # create a toy dataset
     bards_words = ["The fool doth think he is wise,",
@@ -85,22 +104,56 @@ def example_bag():
     print("Dense representation of bag_of_words\n{}".format(
         bag_of_words.toarray()))
 
-def process_movies(text_train, text_test, y_train, y_test):
+def process_movies(text_train, text_test, y_train, y_test, look=True):
     vect = CountVectorizer().fit(text_train)
     X_train = vect.transform(text_train)
     print("X_train:\n{}".format(repr(X_train)))
 
-    # look at vocabulary better
     feature_names = vect.get_feature_names()
-    print("Number of features: {}".format(len(feature_names)))
-    print("First 20 features:\n{}".format(feature_names[:20]))
-    print("Features 20010 to 20030:\n{}".format(feature_names[20010:20030]))
-    print("Every 2000th feature:\n{}".format(feature_names[::2000]))
+
+    if look:
+        # look at vocabulary better
+        print("Number of features: {}".format(len(feature_names)))
+        print("First 20 features:\n{}".format(feature_names[:20]))
+        print("Features 20010 to 20030:\n{}".format(feature_names[20010:20030]))
+        print("Every 2000th feature:\n{}".format(feature_names[::2000]))
 
     # look at crossval with logistic regression
     scores = cross_val_score(LogisticRegression(), X_train, y_train, cv=5)
     print("Mean cross-validation accuracy: {:.2f}".format(np.mean(scores)))
 
-text_train, text_test, y_train, y_test = load_texts()
+    # tune C with crossval
+    param_grid = {'C': [0.001, 0.01, 0.1, 1, 10]}
+    grid = GridSearchCV(LogisticRegression(), param_grid, cv=5)
+    grid.fit(X_train, y_train)
+    print("Best cross-validation score: {:.2f}".format(grid.best_score_))
+    print("Best parameters: ", grid.best_params_)
+
+    # look at generalization performance of parameter setting on test set
+    X_test = vect.transform(text_test)
+    print("{:.2f}".format(grid.score(X_test, y_test)))
+
+def better_process(text_train, text_test, y_train, y_test, look=True):
+    # set the minimum amount of documents a token has to appear in
+    vect = CountVectorizer(min_df=5).fit(text_train)
+    X_train = vect.transform(text_train)
+    print("X_train with min_df: {}".format(repr(X_train)))
+
+    # look at new tokens (they're like a third of the original)
+    feature_names = vect.get_feature_names()
+    if look:
+        print("First 50 features:\n{}".format(feature_names[:50]))
+        print("Features 20020 to 20030:\n{}".format(
+            feature_names[20020:20030]))
+        print("Every 700th feature:\n{}".format(feature_names[::700]))
+
+    # grid search
+    param_grid = {'C': [0.001, 0.01, 0.1, 1, 10]}
+    grid = GridSearchCV(LogisticRegression(), param_grid, cv=5)
+    grid.fit(X_train, y_train)
+    print("Best crossval score: {:.2f}".format(grid.best_score_))
+
+text_train, text_test, y_train, y_test = only_load()
 # example_bag()
-process_movies(text_train, text_test, y_train, y_test)
+# process_movies(text_train, text_test, y_train, y_test)
+better_process(text_train, text_test, y_train, y_test)
